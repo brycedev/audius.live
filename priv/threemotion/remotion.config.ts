2b8253f702b982bd21ff3924ecc3901818faecfb
Bryce Jackson <6643860+brycedev@users.noreply.github.com>
2023-07-13 15:49:39 -0400

    Update remotion.config.ts

--- priv/threemotion/remotion.config.ts
+++ priv/threemotion/remotion.config.ts
@@ -1,5 +1,7 @@
 import {Config} from 'remotion';
+import {CliConfig} from '@remotion/cli/config'
 
 Config.setImageFormat('jpeg');
 Config.setChromiumOpenGlRenderer('angle');
+CliConfig.setChromiumOpenGlRenderer('angle');
 Config.setMaxTimelineTracks(100);
