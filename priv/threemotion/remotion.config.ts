ba703c6b4e7905eee8953674a709b99fb49ecb4a
Bryce <bryce@brycedev.com>
2023-07-20 23:46:46 -0400

    Update remotion.config.ts

--- priv/threemotion/remotion.config.ts
+++ priv/threemotion/remotion.config.ts
@@ -1,5 +1,5 @@
 import {Config} from "@remotion/cli/config"
 
 Config.setVideoImageFormat('jpeg');
-Config.setChromiumOpenGlRenderer('swangle');
+Config.setChromiumOpenGlRenderer('angle');
 Config.setMaxTimelineTracks(100);
