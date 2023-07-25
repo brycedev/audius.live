e4b5d11631f4914a67036b6156f5200b277d2410
Bryce <bryce@brycedev.com>
2023-07-25 01:03:22 -0400

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -6,16 +6,16 @@ git submodule update --remote
 git reset --hard
 git pull --recurse-submodules origin main
 echo "Updating mix deps..."
-/usr/local/bin/mix deps.get --only prod
+mix deps.get --only prod
 echo "Compiling..."
-MIX_ENV=prod /usr/local/bin/mix compile
+MIX_ENV=prod mix compile
 echo "Setting up assets..."
 npm install --prefix assets
-MIX_ENV=prod /usr/local/bin/mix assets.deploy
-MIX_ENV=prod /usr/local/bin/mix phx.digest
+MIX_ENV=prod mix assets.deploy
+MIX_ENV=prod mix phx.digest
 echo "Releasing..."
-source .env && MIX_ENV=prod /usr/local/bin/mix release --overwrite
-MIX_ENV=prod /usr/local/bin/mix ecto.migrate
+source .env && MIX_ENV=prod mix release --overwrite
+MIX_ENV=prod mix ecto.migrate
 echo "Starting server..."
 source .env && _build/prod/rel/audius_live/bin/audius_live daemon
 echo "All done."
